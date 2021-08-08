aad3aa1e5ce4360799041a54c23603aed3ff9473
vishal-daffodil <vishal.joshi@daffodilsw.com>
2021-08-09 00:20:01 +0530

    updates

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,23 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import React, { useContext, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const UserContext = React.createContext()
+const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-  const { loginWithRedirect, logout, user } = useAuth0()
+	const { loginWithRedirect, logout, user } = useAuth0();
 
-  const [myUser, setMyUser] = useState(null)
+	const [myUser, setMyUser] = useState(null);
 
-  useEffect(() => {
-    setMyUser(user)
-  }, [user])
+	useEffect(() => {
+		let userDetails = JSON.parse(localStorage.getItem(user?.nickname)) || {};
+		setMyUser({ ...user, ...userDetails });
+	}, [user]);
 
-  return (
-    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
-      {children}
-    </UserContext.Provider>
-  )
-}
+	return (
+		<UserContext.Provider
+			value={{ loginWithRedirect, logout, myUser, setMyUser }}
+		>
+			{children}
+		</UserContext.Provider>
+	);
+};
 // make sure use
 export const useUserContext = () => {
-  return useContext(UserContext)
-}
+	return useContext(UserContext);
+};
